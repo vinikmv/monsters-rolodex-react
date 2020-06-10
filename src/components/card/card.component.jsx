a8898e5f4d54afce2927f6693e9cc7e5a3d4cbe3
Vinicius Vicente <viniciuskmv@gmail.com>
2020-06-10 11:24:40 -0300

    Commit inicial

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -3,9 +3,9 @@ import React from "react";
 import "./card.styles.css";
 
 export const Card = (props) => (
-  <div className="card-container">
+  <div className='card-container'>
     <img
-      alt="monter"
+      alt='monster'
       src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
     />
     <h2> {props.monster.name}</h2>
